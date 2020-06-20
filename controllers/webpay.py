efc89ddbc00571d96fabf2d108086518fee5beae
Abiezer <ajsifontes@urbos.io>
2020-06-19 23:59:34 -0400

    show webpay response

--- controllers/webpay.py
+++ controllers/webpay.py
@@ -5,12 +5,9 @@ from odoo.http import request
 from odoo.addons.payment_webpay.controllers.main import WebpayController
 
 class CustomWebpayController(WebpayController):
-    @http.route([
-        '/payment/webpay/return/<model("payment.acquirer"):acquirer_id>',
-        '/payment/webpay/test/return',
-    ], type='http', auth='public', csrf=False, website=True)
+    @http.route()
     def webpay_form_feedback(self, acquirer_id=None, **post):
-        resp = super(CustomWebpayController, self).webpay_form_feedback()
+        respuesta = super(CustomWebpayController, self).webpay_form_feedback()
         """ Webpay contacts using GET, at least for accept """
         _logger.warning('Webpay: entering form_feedback with post data %s', pprint.pformat(post))  # debug
         token_ws = post.get('token_ws') or post.get('TBK_TOKEN')
@@ -29,12 +26,19 @@ class CustomWebpayController(WebpayController):
             Puede ser vacío si la transacción no se autenticó.
         '''
         if resp:
+            _logger.info("############################################")
+            _logger.info("############################################")
+            _logger.info("############################################")
+            _logger.info(resp)
+            _logger.info("############################################")
+            _logger.info("############################################")
+            _logger.info("############################################")
             request.env['payment.transaction'].sudo().form_feedback(resp, 'webpay')
-            if str(resp.detailOutput[0].responseCode) in ['0']:
+            if resp.VCI in ['TSY'] and str(resp.detailOutput[0].responseCode) in ['0']:
                 values = {
-                    'url': resp.urlRedirection,
-                    'token_ws': token_ws
-                }
+                            'url': resp.urlRedirection,
+                            'token_ws': token_ws
+                        }
                 return request.render('payment_webpay.webpay_redirect', values)
             request.website.sale_reset()
         elif post.get('TBK_ORDEN_COMPRA'):
@@ -42,4 +46,4 @@ class CustomWebpayController(WebpayController):
                             ('reference', '=', post.get('TBK_ORDEN_COMPRA'))
                             ])
             tx.write({'state': 'error', 'state_message': 'Pago cancelado (abortado en formulario Webpay)'})
-        return resp
+        return respuesta
